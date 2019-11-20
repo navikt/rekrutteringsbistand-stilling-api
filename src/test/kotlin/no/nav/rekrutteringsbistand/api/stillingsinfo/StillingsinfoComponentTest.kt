4efabd09f051a25d8efae1d7009c7e88a26c050b
joar <joar.aurdal@gmail.com>
2019-11-20 12:47:44 +0100

    komponenttester stillingsinfo

--- src/test/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoComponentTest.kt
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoComponentTest.kt
@@ -1,11 +1,17 @@
 package no.nav.rekrutteringsbistand.api.stillingsinfo
 
+import arrow.core.getOrElse
+import no.nav.rekrutteringsbistand.api.Testdata
 import no.nav.rekrutteringsbistand.api.support.toMultiValueMap
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.After
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.client.TestRestTemplate.HttpClientOption.ENABLE_COOKIES
@@ -13,6 +19,7 @@ import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders.ACCEPT
 import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.context.ActiveProfiles
@@ -22,7 +29,7 @@ import java.util.*
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("local")
-class RekrutteringsbisandComponentTest {
+class StillingsinfoComponentTest {
 
     @LocalServerPort
     private var port = 0
@@ -31,27 +38,58 @@ class RekrutteringsbisandComponentTest {
 
     val restTemplate = TestRestTemplate(ENABLE_COOKIES)
 
+    @Autowired
+    lateinit var repository: StillingsinfoRepository
+
     @Before
     fun authenticateClient() {
         restTemplate.getForObject("$localBaseUrl/local/cookie-isso", String::class.java)
     }
 
     @Test
-    fun `Lagring av rekrutteringsbistand skal returnere HTTP status 201 og JSON med nyopprettet rekrutteringUuid`() {
+    @Ignore
+    fun `Henting av rekrutteringsbistand skal returnere HTTP status 200 og JSON med nyopprettet rekrutteringUuid`() {
         // Given
-        val url = "$localBaseUrl/rekruttering"
-        val input = StillingsinfoDto(
-                eierNavident = "anyEierident",
-                eierNavn = "anyEierNavn",
-                stillingsid = "35158853-a09f-4de7-b045-f450ab8e3efc")
+        val lagre = Testdata.enStillingsinfo
+        repository.lagre(lagre)
+
+        val url = "$localBaseUrl/rekruttering/stilling/${lagre.stillingsid}"
+
         val headers = mapOf(
                 CONTENT_TYPE to APPLICATION_JSON.toString(),
                 ACCEPT to APPLICATION_JSON.toString()
         ).toMultiValueMap()
-        val request = HttpEntity(input, headers)
+
+        val httpEntity = HttpEntity(null, headers)
 
         // When
-        val actualResponse = restTemplate.postForEntity(url, request, StillingsinfoDto::class.java)
+        val actualResponse = restTemplate.exchange(url, HttpMethod.GET, httpEntity, StillingsinfoDto::class.java)
+
+        // Then
+        assertThat(actualResponse.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(actualResponse.hasBody())
+        actualResponse.body!!.apply {
+            assertThat(stillingsinfoid).isNotBlank()
+            assertDoesNotThrow { UUID.fromString(stillingsinfoid) }
+            assertThat(this).isEqualTo(lagre.asDto())
+            repository.slett(lagre.stillingsinfoid)
+        }
+    }
+
+    @Test
+    fun `Lagring av rekrutteringsbistand skal returnere HTTP status 201 og JSON med nyopprettet rekrutteringUuid`() {
+        // Given
+        val tilLagring = Testdata.enStillingsinfo.asDto().copy(stillingsinfoid = null)
+        val url = "$localBaseUrl/rekruttering"
+
+        val headers = mapOf(
+                CONTENT_TYPE to APPLICATION_JSON.toString(),
+                ACCEPT to APPLICATION_JSON.toString()
+        ).toMultiValueMap()
+        val httpEntity = HttpEntity(tilLagring, headers)
+
+        // When
+        val actualResponse = restTemplate.postForEntity(url, httpEntity, StillingsinfoDto::class.java)
 
         // Then
         assertThat(actualResponse.statusCode).isEqualTo(HttpStatus.CREATED)
@@ -59,9 +97,54 @@ class RekrutteringsbisandComponentTest {
         actualResponse.body!!.apply {
             assertThat(stillingsinfoid).isNotBlank()
             assertDoesNotThrow { UUID.fromString(stillingsinfoid) }
-            assertThat(stillingsid).isEqualTo(input.stillingsid)
-            assertThat(eierNavident).isEqualTo(input.eierNavident)
-            assertThat(eierNavn).isEqualTo(input.eierNavn)
+            assertThat(this.copy(stillingsinfoid = null))
+                    .isEqualTo(tilLagring)
+        }
+
+        val oppdatert = repository.hentForStilling(Testdata.enStillingsinfo.stillingsid).getOrElse { fail("fant ikke stillingen") }
+        oppdatert.apply {
+            assertThat(stillingsinfoid.asString()).isNotBlank()
+            assertThat(this.asDto())
+                    .isEqualTo(actualResponse.body!!)
+            repository.slett(this.stillingsinfoid)
         }
     }
+
+    @Test
+    fun `Oppdatering av rekrutteringsbistand skal returnere HTTP status 200 og JSON med oppdatert rekrutteringUuid`() {
+
+        // Given
+        val lagre = Testdata.enStillingsinfo
+        val oppdatere = lagre.copy(eier = Eier(navident = "endretIdent", navn = "endretNavn"))
+        val lagret = repository.lagre(lagre)
+
+        val url = "$localBaseUrl/rekruttering"
+
+        val headers = mapOf(
+                CONTENT_TYPE to APPLICATION_JSON.toString(),
+                ACCEPT to APPLICATION_JSON.toString()
+        ).toMultiValueMap()
+        val httpEntity = HttpEntity(oppdatere.asDto(), headers)
+
+        // When
+        val actualResponse = restTemplate.exchange(url, HttpMethod.PUT, httpEntity, StillingsinfoDto::class.java)
+
+        // Then
+        assertThat(actualResponse.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(actualResponse.hasBody())
+        actualResponse.body!!.apply {
+            assertThat(stillingsinfoid).isNotBlank()
+            assertDoesNotThrow { UUID.fromString(stillingsinfoid) }
+            assertThat(this).isEqualTo(oppdatere.asDto())
+        }
+
+        val oppdatert = repository.hentForStilling(oppdatere.stillingsid).getOrElse { fail("fant ikke stillingen") }
+        oppdatert.apply {
+            assertThat(stillingsinfoid.asString()).isNotBlank()
+            assertThat(this)
+                    .isEqualTo(oppdatere)
+            repository.slett(this.stillingsinfoid)
+        }
+    }
+
 }
