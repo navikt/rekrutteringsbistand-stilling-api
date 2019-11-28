261ab2adc20e49a7b553c6d0e97e266f0dd2f467
André Mossige <mossige.a@gmail.com>
2019-11-28 10:28:35 +0100

    Fjern TODO

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
@@ -31,7 +31,6 @@ class StillingsinfoController(
     }
 
     @PutMapping
-    // TODO: Kan man ikke ta inn OppdaterStillingsinfo her?
     fun oppdater(@RequestBody dto: StillingsinfoDto): ResponseEntity<StillingsinfoDto> {
         if (dto.stillingsinfoid == null) throw BadRequestException("Stillingsinfoid må ha verdi for put")
 
