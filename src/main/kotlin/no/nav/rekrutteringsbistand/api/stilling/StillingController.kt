b99c72f1b9d6c2a3e7a39b759e484fbb595ed33a
André Mossige <mossige.a@gmail.com>
2019-12-03 09:36:20 +0100

    Fjern unødvendig httpheadere

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stilling/StillingController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stilling/StillingController.kt
@@ -1,10 +1,8 @@
 package no.nav.rekrutteringsbistand.api.stilling
 
-import no.nav.rekrutteringsbistand.api.support.LOG
 import no.nav.rekrutteringsbistand.api.support.config.Configuration
 import no.nav.rekrutteringsbistand.api.support.config.ExternalConfiguration
 import no.nav.rekrutteringsbistand.api.support.rest.RestProxy
-import no.nav.rekrutteringsbistand.api.support.toMultiValueMap
 import no.nav.security.oidc.api.Protected
 import no.nav.security.oidc.api.Unprotected
 import org.springframework.http.*
@@ -19,16 +17,12 @@ class StillingController(
         val stillingService: StillingService
 ) {
 
-    @PostMapping(
-            "/rekrutteringsbistand/api/v1/ads/"
-    )
+    @PostMapping("/rekrutteringsbistand/api/v1/ads/")
     fun proxyPostTilStillingsApi(request: HttpServletRequest, @RequestBody stilling: Stilling): ResponseEntity<StillingMedStillingsinfo> {
         return ResponseEntity.ok().body(stillingService.opprettStilling(stilling, request.queryString))
     }
 
-    @PutMapping(
-            "/rekrutteringsbistand/api/v1/ads/{uuid}"
-    )
+    @PutMapping("/rekrutteringsbistand/api/v1/ads/{uuid}")
     fun proxyPutTilStillingsApi(@PathVariable uuid: String, request: HttpServletRequest, @RequestBody stilling: Stilling): ResponseEntity<StillingMedStillingsinfo> {
         return ResponseEntity.ok().body(stillingService.oppdaterStilling(uuid, stilling, request.queryString))
     }
@@ -40,12 +34,7 @@ class StillingController(
 
     @RequestMapping("/search-api/**")
     private fun proxySokTilStillingsApi(method: HttpMethod, request: HttpServletRequest, @RequestBody body: String?): ResponseEntity<String> {
-        val response = restProxy.proxyJsonRequest(method, request, Configuration.ROOT_URL, body ?: "", externalConfiguration.stillingApi.url)
-        val headers = mapOf(
-                HttpHeaders.CONTENT_TYPE to MediaType.APPLICATION_JSON_UTF8_VALUE,
-                HttpHeaders.ACCEPT to MediaType.APPLICATION_JSON_UTF8_VALUE
-        ).toMultiValueMap()
-        return ResponseEntity(response.body, headers, response.statusCode)
+        return restProxy.proxyJsonRequest(method, request, Configuration.ROOT_URL, body ?: "", externalConfiguration.stillingApi.url)
     }
 
     @Unprotected // Fordi kandidatsøk har hentet stillinger uten token frem til nå.
