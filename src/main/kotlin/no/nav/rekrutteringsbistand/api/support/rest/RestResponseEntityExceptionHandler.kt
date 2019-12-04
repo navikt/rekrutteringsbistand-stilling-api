b005830c076227b17d8634f83181837e9e35415c
André Mossige <mossige.a@gmail.com>
2019-12-04 10:29:42 +0100

    Rydd i feilhåndtering av controller

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/support/rest/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/support/rest/RestResponseEntityExceptionHandler.kt
@@ -4,7 +4,6 @@ import no.nav.rekrutteringsbistand.api.support.LOG
 import no.nav.security.spring.oidc.validation.interceptor.OIDCUnauthorizedException
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -14,26 +13,24 @@ import org.springframework.web.context.request.ServletWebRequest
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.io.IOException
-import java.nio.file.AccessDeniedException
-import java.util.*
-import javax.servlet.http.HttpServletResponse
 
 @ControllerAdvice
 class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
 
-    @ExceptionHandler(value = [OIDCUnauthorizedException::class, AccessDeniedException::class])
-    @ResponseBody
-    protected fun handleUnauthorizedException(e: RuntimeException, webRequest: WebRequest): ResponseEntity<Any> {
-        val uri = (webRequest as ServletWebRequest).request.requestURI
-        LOG.warn("Authorization exception uri: {}, message: {}", uri, e.message)
-        return getResponseEntity(e, "You are not authorized to access this ressource", HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(OIDCUnauthorizedException::class)
+    protected fun håndterUinnlogget(): ResponseEntity<String> {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body("You are not authorized to access this resource")
     }
 
     @ExceptionHandler(IOException::class, ResourceAccessException::class)
-    @Throws(IOException::class)
-    protected fun handleIOException(e: Exception, response: HttpServletResponse) {
-        LOG.error("IO feil", e)
-        response.sendError(HttpStatus.BAD_GATEWAY.value(), e.message)
+    protected fun håndterIOException(e: Exception, webRequest: WebRequest): ResponseEntity<String> {
+        val uri = (webRequest as ServletWebRequest).request.requestURI
+        LOG.error("Håndterer IOException, uri: $uri", e)
+        return ResponseEntity
+                .status(HttpStatus.BAD_GATEWAY)
+                .body("Connection error")
     }
 
     @ExceptionHandler(value = [EmptyResultDataAccessException::class, NoContentException::class])
@@ -41,24 +38,10 @@ class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
     protected fun handleNoContent(e: RuntimeException, webRequest: WebRequest): ResponseEntity<Any> {
         val uri = (webRequest as ServletWebRequest).request.requestURI
         LOG.error("Ikke innhold: $uri")
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(uri)
-    }
-
-    private fun getResponseEntity(e: RuntimeException, melding: String, status: HttpStatus): ResponseEntity<Any> {
-        val body = HashMap<String, String>(1)
-        body["message"] = melding
-        LOG.info(
-                String.format(
-                        "Returnerer følgende HttpStatus '%s' med melding '%s' pga exception '%s'",
-                        status.toString(),
-                        melding,
-                        e.message
-                )
-        )
-
-        return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(body)
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body(uri)
     }
 
     class NoContentException(message: String?) : RuntimeException(message)
-
 }
