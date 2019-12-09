c70399e1efe753e38ad5b02f5bf61425a112a857
André Mossige <andre.mossige@nav.no>
2019-12-09 10:22:14 +0100

    Feature toggling (#15)
    * Legg til feature toggling med unleash
    
    * Legg til test for featuretoggling
    
    * Flytt featuretoggle navn til konstant

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/TokenUtils.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/TokenUtils.kt
@@ -1,19 +1,48 @@
 package no.nav.rekrutteringsbistand.api.autorisasjon
 
+import java.util.*
 import no.nav.security.oidc.context.OIDCRequestContextHolder
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class TokenUtils @Autowired
-constructor(private val contextHolder: OIDCRequestContextHolder) {
-
-    fun hentOidcToken(): String {
-        return contextHolder.oidcValidationContext.getToken(ISSUER_ISSO).idToken
-    }
+class TokenUtils(private val contextHolder: OIDCRequestContextHolder) {
 
     companion object {
         const val ISSUER_ISSO = "isso"
     }
 
+    fun hentInnloggetVeileder(): InnloggetVeileder {
+        return contextHolder.oidcValidationContext.getClaims(ISSUER_ISSO)
+                .run {
+                    InnloggetVeileder(
+                            userName = get("unique_name"),
+                            displayName = get("name"),
+                            navIdent = get("NAVident")
+                    )
+                }
+    }
+
+    fun tokenUtløper(): Boolean {
+        val hasValidToken = contextHolder.oidcValidationContext.hasValidTokenFor(ISSUER_ISSO)
+        val expirationTime = contextHolder.oidcValidationContext.getClaims(ISSUER_ISSO).claimSet.expirationTime
+        val inFiveMinutes = Date(System.currentTimeMillis() + 5 * 60000)
+        return hasValidToken && inFiveMinutes.after(expirationTime)
+    }
+
+    fun hentOidcToken(): String = contextHolder.oidcValidationContext.getToken(ISSUER_ISSO).idToken
+
+    fun harInnloggingsContext(): Boolean {
+        return try {
+            contextHolder.oidcValidationContext
+            true
+        } catch (exception: IllegalStateException) { // Kaster exception hvis man prøver å hente context utenfor et request initiert av en bruker
+            false
+        }
+    }
 }
+
+data class InnloggetVeileder(
+        val userName: String,
+        val displayName: String,
+        val navIdent: String
+)
