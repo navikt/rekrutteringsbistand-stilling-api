18c3a08fb131a0f41cc3fa4a709600485b866c28
Are Husby <are.husby@nav.no>
2020-12-02 10:00:27 +0100

    Returner object istedenfor type
    Skjønner ikke hvorfor det ikke var kompileringsfeil slik som det var

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/Option.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/Option.kt
@@ -9,4 +9,4 @@ typealias Some<A> = Either.Right<A>
 /**
  * Inspirasjon hentet fra https://github.com/arrow-kt/arrow-core/issues/114
  */
-fun <A> Option(a: A?): Option<A> = if (a == null) Either.Left(Unit) else Either.right(a)
+fun <A> Option(a: A?): Option<A> = if (a == null) Either.left(Unit) else Either.right(a)
