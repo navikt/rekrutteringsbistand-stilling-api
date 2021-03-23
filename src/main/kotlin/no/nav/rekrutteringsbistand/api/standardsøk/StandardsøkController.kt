f58c9d7524ee10c52e899d08d36c428eedfcbf4d
André Mossige <andremossige@fastmail.com>
2021-03-23 13:52:42 +0100

    Skriv tester for lagring av standardsøk

---
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
@@ -0,0 +1,23 @@
+package no.nav.rekrutteringsbistand.api.standardsøk
+
+import org.springframework.stereotype.Controller
+import java.time.LocalDateTime
+
+@Controller
+class StandardsøkController {
+}
+
+data class LagreStandardsøkDto(val søk: String)
+
+data class HentStandardsøkDto(
+        val søk: String,
+        val navIdent: String,
+        val tidspunkt: LocalDateTime
+)
+
+data class LagretStandardsøk(
+        val id: Int,
+        val søk: String,
+        val navIdent: String,
+        val tidspunkt: LocalDateTime
+)
---
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkRepository.kt
@@ -0,0 +1,7 @@
+package no.nav.rekrutteringsbistand.api.standardsøk
+
+import org.springframework.stereotype.Repository
+
+@Repository
+class StandardsøkRepository {
+}
