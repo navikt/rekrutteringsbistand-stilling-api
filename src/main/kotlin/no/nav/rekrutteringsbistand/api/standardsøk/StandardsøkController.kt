2a50ae200b7534fb6ad07b27d52dab7c14dac279
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2021-03-23 15:29:34 +0100

    WIP: Implementer controller og repository for standardsøk

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
@@ -1,10 +1,27 @@
 package no.nav.rekrutteringsbistand.api.standardsøk
 
-import org.springframework.stereotype.Controller
+import no.nav.rekrutteringsbistand.api.autorisasjon.TokenUtils
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
-@Controller
-class StandardsøkController {
+@RestController
+@RequestMapping("/standardsok")
+@Protected
+class StandardsøkController(val standardsøkRepository: StandardsøkRepository, val tokenUtils: TokenUtils) {
+
+    @PutMapping
+    fun lagreStandardsøk(@RequestBody lagreStandardsøkDto: LagreStandardsøkDto): ResponseEntity<HentStandardsøkDto> {
+        val lagretSøk = standardsøkRepository.oppdaterStandardsøk(lagreStandardsøkDto, tokenUtils.hentInnloggetVeileder().navIdent)
+        val returverdi = HentStandardsøkDto(lagretSøk.søk, lagretSøk.navIdent, lagretSøk.tidspunkt)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(returverdi)
+    }
 }
 
 data class LagreStandardsøkDto(val søk: String)
@@ -16,7 +33,7 @@ data class HentStandardsøkDto(
 )
 
 data class LagretStandardsøk(
-        val id: Int,
+        val id: String,
         val søk: String,
         val navIdent: String,
         val tidspunkt: LocalDateTime
--- src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkRepository.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkRepository.kt
@@ -1,7 +1,58 @@
 package no.nav.rekrutteringsbistand.api.standardsøk
 
+import no.nav.rekrutteringsbistand.api.support.LOG
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+import java.time.LocalDateTime
 
 @Repository
-class StandardsøkRepository {
+class StandardsøkRepository(
+        val jdbcTemplate: NamedParameterJdbcTemplate,
+        simpleJdbcInsert: SimpleJdbcInsert
+) {
+    val standardsøkInsert = simpleJdbcInsert.withTableName("lagret_sok").usingGeneratedKeyColumns("id")
+
+    fun oppdaterStandardsøk(lagreStandardsøkDto: LagreStandardsøkDto, navIdent: String): LagretStandardsøk {
+        val lagretStandardsøk = hentStandardsøk(navIdent)
+
+        if (lagretStandardsøk != null) {
+            return lagretStandardsøk
+        }
+
+        lagreStandardsøk(lagreStandardsøkDto, navIdent)
+        val oppdatertStandardsøk = hentStandardsøk(navIdent)
+
+        if (oppdatertStandardsøk != null) {
+            return oppdatertStandardsøk
+        }
+
+        throw Exception("Kunne ikke lagre standardsøk")
+    }
+
+    private fun lagreStandardsøk(lagreStandardsøkDto: LagreStandardsøkDto, navIdent: String) {
+        val id = standardsøkInsert.executeAndReturnKey(mapOf(
+                "nav_ident" to navIdent,
+                "sok" to lagreStandardsøkDto.søk,
+                "tidspunkt" to LocalDateTime.now(),
+        ))
+
+        LOG.info("Lagret med id $id")
+    }
+
+    fun hentStandardsøk(navIdent: String): LagretStandardsøk? {
+        return jdbcTemplate.query(
+                "SELECT * FROM lagret_sok WHERE nav_ident = :nav_ident LIMIT 1",
+                MapSqlParameterSource("nav_ident", navIdent)
+        ) {
+            rs: ResultSet, _: Int -> LagretStandardsøk(
+                    id = rs.getString("id"),
+                    søk = rs.getString("sok"),
+                    navIdent = rs.getString("nav_ident"),
+                    tidspunkt = rs.getTimestamp("tidspunkt").toLocalDateTime()
+            )
+        }.firstOrNull()
+    }
 }
