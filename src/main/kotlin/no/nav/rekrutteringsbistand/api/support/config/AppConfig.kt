ed932bb566b1607e4d47840f7e4f883fb1482c6b
André Mossige <andre.mossige@nav.no>
2021-05-04 14:15:08 +0200

    Fjern CORS-config siden alle kall går via Node
    Fikser forhåpentligvis den sporadiske feilen:
    
    "When allowCredentials is true, allowedOrigins cannot contain the
    special value "*"since that cannot be set on the
    "Access-Control-Allow-Origin" response header. To allow credentials
    to a set of origins, list them explicitly or consider using
    "allowedOriginPatterns" instead."

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/AppConfig.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/AppConfig.kt
@@ -1,49 +1,16 @@
 package no.nav.rekrutteringsbistand.api.support.config
 
-import no.nav.rekrutteringsbistand.api.support.rest.HeaderFilter
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.Ordered
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
 import java.nio.charset.StandardCharsets
 import java.time.Duration
 
 
 @Configuration
 class AppConfig {
-
-    @Bean
-    fun corsServletFilterRegistration(): FilterRegistrationBean<*> =
-            FilterRegistrationBean<CorsFilter>().apply {
-                filter = CorsFilter(
-                        UrlBasedCorsConfigurationSource()
-                                .apply {
-                                    registerCorsConfiguration("/**", CorsConfiguration().apply {
-                                        addAllowedOrigin("*")
-                                        addAllowedMethod("*")
-                                        allowCredentials = true
-                                        maxAge = 4000L
-                                        addAllowedHeader("*")
-                                    })
-                                })
-                order = Ordered.HIGHEST_PRECEDENCE
-                urlPatterns = setOf("/*")
-                isEnabled = true
-            }
-
-    @Bean
-    fun headerFilterRegistration(): FilterRegistrationBean<*> =
-            FilterRegistrationBean<HeaderFilter>().apply {
-                setFilter(HeaderFilter())
-                urlPatterns = listOf("/rekrutteringsbistand/api/*")
-                setEnabled(true)
-            }
 
     @Bean
     fun restTemplate(): RestTemplate {
