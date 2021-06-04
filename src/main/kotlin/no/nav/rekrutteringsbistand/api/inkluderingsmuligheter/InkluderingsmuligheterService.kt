88fcb5ac3378f349c0a7fb84995a387c4d718f36
Joar Aurdal <joar.aurdal@nav.no>
2021-06-04 09:56:54 +0200

    Legg til masse logging på inkluderingstransformasjon

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/inkluderingsmuligheter/InkluderingsmuligheterService.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/inkluderingsmuligheter/InkluderingsmuligheterService.kt
@@ -14,6 +14,7 @@ class InkluderingsmuligheterService(private val inkluderingsmuligheterRepository
 
 
         val inkluderingsmuligheter = ad.toInkluderingsmuligheter()
+
         val sisteErLik = sisteErLik(inkluderingsmuligheter)
 
         LOG.info("Skal behandle ad ${ad.uuid} properties: ${ad.properties}")
@@ -26,33 +27,52 @@ class InkluderingsmuligheterService(private val inkluderingsmuligheterRepository
     }
 
     fun sisteErLik(inkluderingsmuligheter: Inkluderingsmulighet): Boolean {
-        val inkluderingsmulighet = inkluderingsmuligheterRepository.hentSisteInkluderingsmulighet(inkluderingsmuligheter.stillingsid)
-        return (inkluderingsmulighet == null && !inkluderingsmuligheter.harInkludering()) || (inkluderingsmulighet != null && inkluderingsmuligheter.erLik(inkluderingsmulighet))
+        val inkluderingsmulighet =
+            inkluderingsmuligheterRepository.hentSisteInkluderingsmulighet(inkluderingsmuligheter.stillingsid)
+        return (inkluderingsmulighet == null && !inkluderingsmuligheter.harInkludering()) || (inkluderingsmulighet != null && inkluderingsmuligheter.erLik(
+            inkluderingsmulighet
+        ))
     }
 
     private fun Ad.toInkluderingsmuligheter(): Inkluderingsmulighet {
-
+        LOG.info("toInkluderingsmuligheter $uuid start for ${this.properties}")
         val tagStrings = this.properties.filter { it.key == "tags" }
-        if (tagStrings.isEmpty()) return Inkluderingsmulighet(
+        LOG.info("toInkluderingsmuligheter $uuid tagstring $tagStrings")
+        if (tagStrings.isEmpty()) {
+            LOG.info("toInkluderingsmuligheter ${uuid} tagstrings er empty, lager tom")
+
+            val retur = Inkluderingsmulighet(
+                stillingsid = this.uuid.toString(),
+                radOpprettet = LocalDateTime.now()
+            )
+            LOG.info("toInkluderingsmuligheter ${uuid} tagstrings er empty, returnerer $retur")
+            return retur
+        }
+        LOG.info("toInkluderingsmuligheter ${uuid} tagstrings har verdi")
+
+        val førsteTagstring = tagStrings.first()
+        LOG.info("toInkluderingsmuligheter ${uuid} first: $førsteTagstring, verdi:${førsteTagstring.value} string: ${førsteTagstring.value.toString()}")
+
+        val tags: List<String> = ObjectMapper().readValue(førsteTagstring.value.toString())
+        LOG.info("toInkluderingsmuligheter ${uuid} tags: $tags")
+
+
+        val retur =  Inkluderingsmulighet(
             stillingsid = this.uuid.toString(),
+            tilretteleggingmuligheter = transformerTilNyttFormat(tags, "INKLUDERING"),
+            virkemidler = transformerTilNyttFormat(tags, "TILTAK_ELLER_VIRKEMIDDEL"),
+            prioriterteMålgrupper = transformerTilNyttFormat(tags, "PRIORITERT_MÅLGRUPPE"),
+            statligInkluderingsdugnad = tags.contains("STATLIG_INKLUDERINGSDUGNAD"),
             radOpprettet = LocalDateTime.now()
         )
+        LOG.info("toInkluderingsmuligheter ${uuid} retur: $retur")
 
-        val tags: List<String> = ObjectMapper().readValue(tagStrings.first().value.toString())
-
-        return Inkluderingsmulighet(
-                stillingsid = this.uuid.toString(),
-                tilretteleggingmuligheter = transformerTilNyttFormat(tags, "INKLUDERING"),
-                virkemidler = transformerTilNyttFormat(tags, "TILTAK_ELLER_VIRKEMIDDEL"),
-                prioriterteMålgrupper = transformerTilNyttFormat(tags, "PRIORITERT_MÅLGRUPPE"),
-                statligInkluderingsdugnad = tags.contains("STATLIG_INKLUDERINGSDUGNAD"),
-                radOpprettet = LocalDateTime.now()
-        )
+        return retur
     }
 
     private fun transformerTilNyttFormat(tags: List<String>, kategori: String): List<String> {
         return tags
-                .filter { it.startsWith(kategori) }
-                .map { if (it == kategori) "${it}_KATEGORI" else it.removePrefix("${kategori}__") }
+            .filter { it.startsWith(kategori) }
+            .map { if (it == kategori) "${it}_KATEGORI" else it.removePrefix("${kategori}__") }
     }
 }
