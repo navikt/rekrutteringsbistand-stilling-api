eb67ce041273a70b90906a4db298cee8033b582a
André Mossige <andre.mossige@nav.no>
2021-06-21 08:23:53 +0200

    Slå av Kafka-konsument for inkluderingsmuligheter

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/inkluderingsmuligheter/StillingConsumer.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/inkluderingsmuligheter/StillingConsumer.kt
@@ -17,41 +17,41 @@ class StillingConsumer(
     private val inkluderingsmuligheterService: InkluderingsmuligheterService
 ) : Closeable {
 
-    @Scheduled(fixedRate = Long.MAX_VALUE) // Kjøres kun en gang, i egen tråd/task, ved startup
-    fun start() {
-        try {
-            consumer.subscribe(listOf(stillingstopic))
-
-            LOG.info("Starter å konsumere topic $stillingstopic med groupId ${consumer.groupMetadata().groupId()}")
-
-            while (true) {
-                val records: ConsumerRecords<String, Ad> = consumer.poll(Duration.ofSeconds(5))
-                if (records.count() == 0) continue
-
-                val stilling = records.map { it.value() }
-                val stillingsIder = stilling.map { it.uuid }
-                LOG.info("Stillinger mottatt: $stillingsIder")
-
-                stilling.forEach {
-                    inkluderingsmuligheterService.lagreInkluderingsmuligheter(it)
-                }
-                consumer.commitSync()
-
-                LOG.info("Committet offset ${records.last().offset()} til Kafka")
-            }
-        } catch (exception: WakeupException) {
-            LOG.info("Fikk beskjed om å lukke consument med groupId ${consumer.groupMetadata().groupId()}")
-        } catch (exception: Exception) {
-            LOG.error("Noe galt skjedde i konsument, stopper stilling-konsument", exception)
-        } finally {
-            consumer.close()
-        }
-    }
-
+//    @Scheduled(fixedRate = Long.MAX_VALUE) // Kjøres kun en gang, i egen tråd/task, ved startup
+//    fun start() {
+//        try {
+//            consumer.subscribe(listOf(stillingstopic))
+//
+//            LOG.info("Starter å konsumere topic $stillingstopic med groupId ${consumer.groupMetadata().groupId()}")
+//
+//            while (true) {
+//                val records: ConsumerRecords<String, Ad> = consumer.poll(Duration.ofSeconds(5))
+//                if (records.count() == 0) continue
+//
+//                val stilling = records.map { it.value() }
+//                val stillingsIder = stilling.map { it.uuid }
+//                LOG.info("Stillinger mottatt: $stillingsIder")
+//
+//                stilling.forEach {
+//                    inkluderingsmuligheterService.lagreInkluderingsmuligheter(it)
+//                }
+//                consumer.commitSync()
+//
+//                LOG.info("Committet offset ${records.last().offset()} til Kafka")
+//            }
+//        } catch (exception: WakeupException) {
+//            LOG.info("Fikk beskjed om å lukke consument med groupId ${consumer.groupMetadata().groupId()}")
+//        } catch (exception: Exception) {
+//            LOG.error("Noe galt skjedde i konsument, stopper stilling-konsument", exception)
+//        } finally {
+//            consumer.close()
+//        }
+//    }
+//
     @PreDestroy
     override fun close() {
         // Vil kaste WakeupException i konsument slik at den stopper, thread-safe.
-        LOG.info("Kaller wakeup for topic $stillingstopic")
-        consumer.wakeup()
+//        LOG.info("Kaller wakeup for topic $stillingstopic")
+//        consumer.wakeup()
     }
 }
