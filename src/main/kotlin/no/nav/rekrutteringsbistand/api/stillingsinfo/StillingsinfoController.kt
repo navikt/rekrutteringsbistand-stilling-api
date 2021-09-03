06798b074f8f20d1b5d8b84e10182c25f60f8d53
André Mossige <andre.mossige@nav.no>
2021-09-03 08:44:47 +0200

    Fiks navn på controller

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
@@ -13,7 +13,7 @@ import java.util.*
 @RestController
 @RequestMapping("/rekruttering")
 @ProtectedWithClaims(issuer = "isso")
-class EierController(
+class StillingsinfoController(
         val repo: StillingsinfoRepository,
         val kandidatlisteKlient: KandidatlisteKlient
 ) {
