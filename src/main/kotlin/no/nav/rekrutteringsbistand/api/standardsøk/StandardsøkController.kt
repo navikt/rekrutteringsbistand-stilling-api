429526174d870f86d22bcfddf47ddd634662ac70
madsleegiil <mads.lee.giil@nav.no>
2022-02-14 08:48:21 +0100

    Endre token-issuer for lagret søk

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
@@ -11,7 +11,7 @@ import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/standardsok")
-@ProtectedWithClaims(issuer = "isso")
+@ProtectedWithClaims(issuer = "azuread")
 class StandardsøkController(val standardsøkService: StandardsøkService, val tokenUtils: TokenUtils) {
 
     @PutMapping
