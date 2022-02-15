90474e82c65dcb814dbc1bfd4cb49a75dfd3c392
madsleegiil <mads.lee.giil@nav.no>
2022-02-15 10:10:10 +0100

    Spesifiser eksplisitt hvilke issuers som er godtatt på endepunkt

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/standardsøk/StandardsøkController.kt
@@ -1,7 +1,8 @@
 package no.nav.rekrutteringsbistand.api.standardsøk
 
 import no.nav.rekrutteringsbistand.api.autorisasjon.TokenUtils
-import no.nav.security.token.support.core.api.Protected
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.RequiredIssuers
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -10,21 +11,27 @@ import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/standardsok")
-@Protected
+@RequiredIssuers(
+    value = [
+        ProtectedWithClaims(issuer = "isso"),
+        ProtectedWithClaims(issuer = "azuread")
+    ]
+)
 class StandardsøkController(val standardsøkService: StandardsøkService, val tokenUtils: TokenUtils) {
 
     @PutMapping
     fun opprettEllerOppdaterStandardsøk(@RequestBody lagreStandardsøkDto: LagreStandardsøkDto): ResponseEntity<Any> {
-        val lagretSøk = standardsøkService.oppdaterStandardsøk(lagreStandardsøkDto, tokenUtils.hentInnloggetVeileder().navIdent)
+        val lagretSøk =
+            standardsøkService.oppdaterStandardsøk(lagreStandardsøkDto, tokenUtils.hentInnloggetVeileder().navIdent)
         if (lagretSøk != null) {
             return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(HentStandardsøkDto(lagretSøk.søk, lagretSøk.navIdent, lagretSøk.tidspunkt))
+                .status(HttpStatus.CREATED)
+                .body(HentStandardsøkDto(lagretSøk.søk, lagretSøk.navIdent, lagretSøk.tidspunkt))
         }
 
         return ResponseEntity
-                .status(500)
-                .body("Kunne ikke lagre standardsøk")
+            .status(500)
+            .body("Kunne ikke lagre standardsøk")
     }
 
     @GetMapping
@@ -33,37 +40,37 @@ class StandardsøkController(val standardsøkService: StandardsøkService, val t
         val lagretSøk = standardsøkService.hentStandardsøk(navIdent)
         if (lagretSøk != null) {
             return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(HentStandardsøkDto(lagretSøk.søk, lagretSøk.navIdent, lagretSøk.tidspunkt))
+                .status(HttpStatus.OK)
+                .body(HentStandardsøkDto(lagretSøk.søk, lagretSøk.navIdent, lagretSøk.tidspunkt))
         }
 
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body("Fant ikke standardsøk på gitt ident $navIdent")
+            .status(HttpStatus.NOT_FOUND)
+            .body("Fant ikke standardsøk på gitt ident $navIdent")
     }
 }
 
 data class LagreStandardsøkDto(val søk: String)
 
 data class HentStandardsøkDto(
-        val søk: String,
-        val navIdent: String,
-        val tidspunkt: LocalDateTime
+    val søk: String,
+    val navIdent: String,
+    val tidspunkt: LocalDateTime
 )
 
 data class LagretStandardsøk(
-        val id: String,
-        val søk: String,
-        val navIdent: String,
-        val tidspunkt: LocalDateTime
+    val id: String,
+    val søk: String,
+    val navIdent: String,
+    val tidspunkt: LocalDateTime
 ) {
     companion object {
         fun fromDB(rs: ResultSet) =
-                LagretStandardsøk(
-                        id = rs.getString("id"),
-                        søk = rs.getString("sok"),
-                        navIdent = rs.getString("nav_ident"),
-                        tidspunkt = rs.getTimestamp("tidspunkt").toLocalDateTime()
-                )
+            LagretStandardsøk(
+                id = rs.getString("id"),
+                søk = rs.getString("sok"),
+                navIdent = rs.getString("nav_ident"),
+                tidspunkt = rs.getTimestamp("tidspunkt").toLocalDateTime()
+            )
     }
 }
