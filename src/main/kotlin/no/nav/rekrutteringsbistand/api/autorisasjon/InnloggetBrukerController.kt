8aa38270d11a15b9314c5f31f2b18f89ed56d845
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-02-23 14:38:58 +0100

    Tillat azure-autentisering i flere endepunkt

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/InnloggetBrukerController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/InnloggetBrukerController.kt
@@ -1,13 +1,19 @@
 package no.nav.rekrutteringsbistand.api.autorisasjon
 
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.RequiredIssuers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/rekrutteringsbistand/api/v1/reportee")
-@ProtectedWithClaims(issuer = "isso")
+@RequiredIssuers(
+    value = [
+        ProtectedWithClaims(issuer = "isso"),
+        ProtectedWithClaims(issuer = "azuread")
+    ]
+)
 class InnloggetBrukerController(private val tokenUtils: TokenUtils) {
 
     @GetMapping
