e86cd72bb6c96d0f0645f24db8052ecaca4f4adc
madsleegiil <mads.lee.giil@nav.no>
2022-03-18 09:11:31 +0100

    Opprydding

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/InnloggetBrukerController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/autorisasjon/InnloggetBrukerController.kt
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/rekrutteringsbistand/api/v1/reportee")
-// @ProtectedWithClaims(issuer = "azuread", claimMap = ["NAVident=*", "name=*"]) TODO: Oppgrader bibliotek
 @Protected
 class InnloggetBrukerController(private val tokenUtils: TokenUtils) {
 
--- src/test/kotlin/no/nav/rekrutteringsbistand/api/config/MockLogin.kt
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/config/MockLogin.kt
@@ -6,14 +6,12 @@ import no.nav.security.mock.oauth2.token.DefaultOAuth2TokenCallback
 import no.nav.security.token.support.spring.test.MockOAuth2ServerAutoConfiguration
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Import
-import org.springframework.http.HttpRequest
-import org.springframework.http.client.ClientHttpRequestExecution
 import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Component
 
 @Import(MockOAuth2ServerAutoConfiguration::class)
-@RestController
-class MockLogin(val mockOauth2Server: MockOAuth2Server) {
+@Component
+class MockLogin(private val mockOauth2Server: MockOAuth2Server) {
 
     fun hentAzureAdVeilederToken(): String {
         return mockOauth2Server.issueToken(
@@ -38,9 +36,9 @@ class MockLogin(val mockOauth2Server: MockOAuth2Server) {
 
     fun hentAzureAdMaskinTilMaskinToken(clientId: String): String {
         return mockOauth2Server.issueToken(
-                azureAdIssuer,
-                clientId,
-                DefaultOAuth2TokenCallback()
+            azureAdIssuer,
+            clientId,
+            DefaultOAuth2TokenCallback()
         ).serialize()
     }
 }
