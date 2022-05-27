30cb1c3a1330b7fd908996d37bf7f7e4e53b057d
Joar Aurdal <joar.aurdal@nav.no>
2022-05-27 15:08:41 +0200

    Bytt til mer springaktig datasource config

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/DatabaseConfig.kt
@@ -3,6 +3,7 @@ package no.nav.rekrutteringsbistand.api.support.config
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
@@ -12,49 +13,18 @@ import org.springframework.context.annotation.Profile
 import javax.sql.DataSource
 
 @Configuration
-@Profile("dev", "prod")
 class DatabaseConfig {
 
-    @Value("\${database.hostname}")
-    private val databaseHostname: String? = null
-
-    @Value("\${database.port}")
-    private val databasePort: String? = null
-
-    @Value("\${database.navn}")
-    private val databaseNavn: String? = null
-
-    @Value("\${database.brukernavn}")
-    private val databaseBrukernavn: String? = null
-
-    @Value("\${database.passord}")
-    private val databasePassord: String? = null
-
-    private fun dataSource(): HikariDataSource {
-        val config = HikariConfig()
-        config.jdbcUrl = "jdbc:postgresql://$databaseHostname:$databasePort/$databaseNavn"
-        config.maximumPoolSize = 2
-        config.minimumIdle = 1
-        config.driverClassName = "org.postgresql.Driver"
-        config.initializationFailTimeout = 5000
-        config.username = databaseBrukernavn
-        config.password = databasePassord
-        config.validate()
-        return HikariDataSource(config)
-    }
+    @Autowired
+    private lateinit var dataSource: DataSource
 
     @Bean
     fun flywayMigrationStrategy(): FlywayMigrationStrategy {
         return FlywayMigrationStrategy {
             Flyway.configure()
-                .dataSource(dataSource())
+                .dataSource(dataSource)
                 .load()
                 .migrate()
         }
     }
-
-    private fun dbRole(role: String): String {
-        return arrayOf(databaseNavn, role).joinToString("-")
-    }
-
 }
