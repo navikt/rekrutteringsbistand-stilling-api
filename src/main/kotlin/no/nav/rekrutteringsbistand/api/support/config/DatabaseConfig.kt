edd87107cbcb4336043a23959c29f9380cbf44b7
Joar Aurdal <joar.aurdal@nav.no>
2022-05-30 11:30:25 +0200

    Legg inn flyway

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/DatabaseConfig.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/support/config/DatabaseConfig.kt
@@ -1,14 +1,10 @@
 package no.nav.rekrutteringsbistand.api.support.config
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
-//import org.flywaydb.core.Flyway
+import org.flywaydb.core.Flyway
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 import javax.sql.DataSource
 
@@ -18,13 +14,10 @@ class DatabaseConfig {
     @Autowired
     private lateinit var dataSource: DataSource
 
-    /*@Bean
+    @Bean
     fun flywayMigrationStrategy(): FlywayMigrationStrategy {
         return FlywayMigrationStrategy {
-            Flyway.configure()
-                .dataSource(dataSource)
-                .load()
-                .migrate()
+            Flyway.configure().dataSource(dataSource).load().migrate()
         }
-    }*/
+    }
 }
