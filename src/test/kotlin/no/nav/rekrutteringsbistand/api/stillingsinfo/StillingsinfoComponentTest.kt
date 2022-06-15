9d8cfb725dfa76a648821a354e8012e19dd30c70
Mads Lee Giil <mads.lee.giil@nav.no>
2022-06-15 20:59:19 +0200

    Endre testsignaturer og legg inn fail fordi de ikke er korrekte ennå

--- src/test/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoComponentTest.kt
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoComponentTest.kt
@@ -19,6 +19,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
+import org.mockito.Spy
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -124,7 +125,7 @@ class StillingsinfoComponentTest {
     }
 
     @Test
-    fun `Oppretting av kandidatliste på ekstern stilling skal returnere 500 og ikke lagre noe i databasen hvis kall mot kandidat-api feiler`() {
+    fun `Oppretting av kandidatliste på ekstern stilling skal lagre stillingsinfo for så å reversere lagring og returnere 500 når kall mot kandidat-api feiler`() {
         val dto = enStillingsinfoInboundDto
         mockAzureObo(wiremockAzure)
         `when`(kandidatlisteKlient.varsleOmOppdatertStilling(Stillingsid(dto.stillingsid))).thenThrow(RuntimeException::class.java)
@@ -135,10 +136,11 @@ class StillingsinfoComponentTest {
         assertThat(respons.statusCodeValue).isEqualTo(500)
         val stillingsinfo = repository.hentForStilling(Stillingsid(dto.stillingsid)).orNull()
         assertThat(stillingsinfo).isNull()
+        fail<String>("TODO: endre test til å verifie kall mot database")
     }
 
     @Test
-    fun `Endring av eier av kandidatliste for ekstern stilling skal returnere 500 og ikke lagre endringer i databasen hvis kall mot kandidat-api feiler`() {
+    fun `Endring av eier av kandidatliste for ekstern stilling skal lagre endring for så å reversere endringen og returnere 500 når kall mot kandidat-api feiler`() {
         val stillingsinfo = enStillingsinfo.copy(eier = Eier("Y111111", "Et Navn"))
         repository.opprett(stillingsinfo)
         val endringDto = StillingsinfoInboundDto(
@@ -154,6 +156,7 @@ class StillingsinfoComponentTest {
         assertThat(respons.statusCodeValue).isEqualTo(500)
         val lagretStillingsinfo = repository.hentForStilling(stillingsinfo.stillingsid).orNull()
         assertThat(lagretStillingsinfo!!.eier).isEqualTo(stillingsinfo.eier)
+        fail<String>("TODO: endre test til å verifie kall mot database")
     }
 
     @After
