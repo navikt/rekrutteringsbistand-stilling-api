62e6bcd5477e0e837f0cda069aa32eea13a75fd9
Are Husby <are.husby@nav.no>
2022-11-10 15:04:34 +0100

    Mer logging for å finne årsak til prodfeil RecordTooLargeException

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/hendelser/StillingsinfoPopulator.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/hendelser/StillingsinfoPopulator.kt
@@ -1,9 +1,12 @@
 package no.nav.rekrutteringsbistand.api.hendelser
 
-import com.fasterxml.jackson.databind.JsonNode
-import no.nav.helse.rapids_rivers.*
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
 import no.nav.rekrutteringsbistand.api.arbeidsplassen.ArbeidsplassenKlient
 import no.nav.rekrutteringsbistand.api.stillingsinfo.*
+import no.nav.rekrutteringsbistand.api.support.log
 
 class StillingsinfoPopulator(
     rapidsConnection: RapidsConnection,
@@ -25,7 +28,23 @@ class StillingsinfoPopulator(
         arbeidsplassenKlient.hentStillingBasertPåUUID(stillingsId).map {
             packet["stilling"] = Stilling(it.title)
         }
-        context.publish(packet.toJson())
+        val message: String = packet.toJson()
+
+        /**
+         * Sjekk for å unngå at følgende exception logges men IKKE kastes, og som derfor lar systemet fortsette å konsumere nye meldinger,
+         * og det blir feil data sendt til DVH/team Arbeidsmarkedsdata
+         *
+         * Kibana log message: `Shutting down rapid due to fatal error: The message is 1048751 bytes when serialized which is larger than 1048576, which is the value of the max.request.size configuration.`
+         *
+         * Kibana stacktrace: `org.apache.kafka.common.errors.RecordTooLargeException: The message is 1048751 bytes when serialized which is larger than 1048576, which is the value of the max.request.size configuration.`
+         */
+        val kafkaMaxRequestSizeBytes = 1048576
+        val messageSizeBytes = message.toByteArray().size
+        if (messageSizeBytes > kafkaMaxRequestSizeBytes) {
+            log.warn("Utgående melding kan være for stor til å bli sendt. stillingsId=$stillingsId, messageSizeBytes=$messageSizeBytes, kafkaMaxRequestSizeBytes=$kafkaMaxRequestSizeBytes")
+        }
+
+        context.publish(message)
     }
 }
 
