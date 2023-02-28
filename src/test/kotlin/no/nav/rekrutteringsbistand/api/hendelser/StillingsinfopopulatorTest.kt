a0105bb0630c2bcadad1fa7cdc6468ab63f4a185
frodank <11638682+frodank@users.noreply.github.com>
2023-02-28 14:01:26 +0100

    Legger til felt på test som mangler (test-feil)

--- src/test/kotlin/no/nav/rekrutteringsbistand/api/hendelser/StillingsinfopopulatorTest.kt
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/hendelser/StillingsinfopopulatorTest.kt
@@ -7,6 +7,7 @@ import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.rekrutteringsbistand.api.Testdata.enStilling
 import no.nav.rekrutteringsbistand.api.arbeidsplassen.ArbeidsplassenKlient
 import no.nav.rekrutteringsbistand.api.hendelser.RapidApplikasjon.Companion.registrerLyttere
+import no.nav.rekrutteringsbistand.api.stilling.Arbeidsgiver
 import no.nav.rekrutteringsbistand.api.stillingsinfo.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -19,6 +20,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.ApplicationContext
 import org.springframework.test.context.junit4.SpringRunner
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.util.*
 
 @RunWith(SpringRunner::class)
@@ -49,9 +53,30 @@ class StillingsinfopopulatorTest {
         val stillingsId = Stillingsid(UUID.randomUUID())
         val stillingsTittel = "Klovn på sirkus"
         val eksternStillingskilde = "ASS"
+        val stillingstidspunkt = "tidspunktpublishedByAdmin"
+        val antallStillinger = 666
+        val organisasjonsnummer = "123"
+        val stillingensPubliseringstidspunkt = LocalDateTime.now()
 
         Mockito.`when`(arbeidsplassenKlient.hentStillingBasertPåUUID(stillingsId.toString()))
-            .thenReturn(Some(enStilling.copy(title = stillingsTittel, source = eksternStillingskilde)))
+            .thenReturn(
+                Some(
+                    enStilling.copy(
+                        title = stillingsTittel,
+                        source = eksternStillingskilde,
+                        publishedByAdmin = stillingstidspunkt,
+                        properties = mapOf("positioncount" to "$antallStillinger"),
+                        employer = Arbeidsgiver(
+                            null, null, null, null, null,
+                            null, emptyList(), emptyList(), null, emptyList(),
+                            emptyMap(), null,
+                            organisasjonsnummer,
+                            null, null, null, null, null, null
+                        ),
+                        published = stillingensPubliseringstidspunkt
+                    )
+                )
+            )
         val stillingsinfoid = Stillingsinfoid(UUID.randomUUID())
         val eier = Eier("AB123456", "Navnesen")
         val notat = "Et notat"
@@ -73,6 +98,14 @@ class StillingsinfopopulatorTest {
         assertEquals("felt2", message.path("uinteressant2").asText())
         assertEquals(stillingsId.asString(), message.path("stillingsId").asText())
         assertEquals(stillingsTittel, message.path("stilling").get("stillingstittel").asText())
+        assertEquals(stillingsTittel=="DIR", message.path("stilling").get("erDirektemeldt").asBoolean())
+        assertEquals(stillingstidspunkt, message.path("stilling").get("stillingOpprettetTidspunkt").asText())
+        assertEquals(antallStillinger, message.path("stilling").get("antallStillinger").asInt())
+        assertEquals(organisasjonsnummer, message.path("stilling").get("organisasjonsnummer").asText())
+        assertEquals(
+            ZonedDateTime.of(stillingensPubliseringstidspunkt, ZoneId.of("Europe/Oslo")),
+            message.path("stilling").get("stillingensPubliseringstidspunkt").asText()
+        )
         assertFalse(message.path("stilling").get("erDirektemeldt").asBoolean())
         val stillingNode = message.path("stillingsinfo")
         assertFalse(stillingNode.isMissingOrNull())
