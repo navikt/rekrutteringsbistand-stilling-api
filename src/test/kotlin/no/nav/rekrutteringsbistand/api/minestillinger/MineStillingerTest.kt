8f7b7239497e60c4b868ed2c1ad4540000277f6c
Mads Lee Giil <mads.lee.giil@nav.no>
2023-05-12 14:38:12 +0200

    Definer test cases

---
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/minestillinger/MineStillingerTest.kt
@@ -0,0 +1,17 @@
+package no.nav.rekrutteringsbistand.api.minestillinger
+
+class MineStillingerTest {
+
+    /*
+    Test cases:
+    - Når veileder oppretter en direktemeldt stilling, så skal vi lagre det vi trenger for å vise den fram i Mine stillinger
+    - Når veileder oppretter en direktemeldt stilling, så skal vi lagre annonsenummeret fra pam-ad-api
+    - Når veileder lager kandidatliste for en ekstern stilling, så skal vi lagre det vi trenger for å vise den fram i Mine stillinger
+    - Når veileder oppdaterer en direktemelding stilling, så skal vi lagre de oppdaterte verdiene
+    - Når stilling-api konsumerer en melding som gjelder en ekstern stilling vi allerede har lagret data på, så skal vi oppdatere verdiene
+    - Når veileder sletter en direktemeldt stilling, så skal vi slette den fra tabellen
+    - Når stilling-api konsumerer en melding om en slettet stilling, så skal vi slette stillingen
+    - Når veileder overtar en direktemeldt stilling, skal vi oppdatere eier i tabellen
+    - Når veileder overtar en ekstern stilling, skal vi oppdatere eier i tabellen
+     */
+}
