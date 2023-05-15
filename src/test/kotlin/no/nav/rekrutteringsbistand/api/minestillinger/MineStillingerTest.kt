18cacee6bb6902e33c5a43d1d27b9036a946e42c
Are Husby <are.husby@nav.no>
2023-05-15 17:28:52 +0200

    Slett kommentar testcase

--- src/test/kotlin/no/nav/rekrutteringsbistand/api/minestillinger/MineStillingerTest.kt
+++ src/test/kotlin/no/nav/rekrutteringsbistand/api/minestillinger/MineStillingerTest.kt
@@ -211,7 +211,6 @@ class MineStillingerTest {
 
     /*
     Test cases:
-    - Når veileder lager kandidatliste for en ekstern stilling, så skal vi lagre det vi trenger for å vise den fram i Mine stillinger
     - Når stilling-api konsumerer en melding som gjelder en ekstern stilling vi allerede har lagret data på, så skal vi oppdatere verdiene
     - Når veileder sletter en direktemeldt stilling, så skal vi slette den fra tabellen
     - Når stilling-api konsumerer en melding om en slettet stilling, så skal vi slette stillingen
