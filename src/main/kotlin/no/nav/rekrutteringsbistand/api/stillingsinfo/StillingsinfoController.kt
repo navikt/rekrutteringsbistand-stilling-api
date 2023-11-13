90519880c5722e3a932fe3d2d7ce89e4de0a113f
Joar Aurdal <joar.aurdal@nav.no>
2023-11-13 15:57:49 +0100

    Legg på forrige eier i logg for eksterne stillinger som overtas

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoController.kt
@@ -1,11 +1,13 @@
 package no.nav.rekrutteringsbistand.api.stillingsinfo
 
-import no.nav.rekrutteringsbistand.api.support.log
 import no.nav.rekrutteringsbistand.api.support.secureLog
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/stillingsinfo")
@@ -17,9 +19,15 @@ class StillingsinfoController(
     fun overtaEierskapForEksternStillingOgKandidatliste(
         @RequestBody dto: StillingsinfoInboundDto
     ): ResponseEntity<StillingsinfoDto> {
-        secureLog.info("Overtar ekstern stilling og kandidatliste ident ${dto.eierNavident} stillingsid ${dto.stillingsid}")
+        val stillingsid = Stillingsid(dto.stillingsid)
+
+        val forrigeStillingsinfo = service.hentForStilling(stillingId = stillingsid)
+        val forrigeEier = forrigeStillingsinfo.orNull()?.eier?.navident
+
+        secureLog.info("Overtar ekstern stilling og kandidatliste gammel eier $forrigeEier ny eier ${dto.eierNavident} stillingsid ${dto.stillingsid} (under utvikling)")
         val nyEier = Eier(dto.eierNavident, dto.eierNavn)
-        val oppdatertStillingsinfo = service.overtaEierskapForEksternStillingOgKandidatliste(Stillingsid(dto.stillingsid), nyEier)
+        val oppdatertStillingsinfo =
+            service.overtaEierskapForEksternStillingOgKandidatliste(stillingsId = stillingsid, nyEier = nyEier)
 
         return ResponseEntity.status(HttpStatus.OK).body(oppdatertStillingsinfo.asStillingsinfoDto())
     }
