377e82f2e9303edd3e06304eff74b4306a73659c
Joar Aurdal <joar.aurdal@nav.no>
2024-10-15 10:56:35 +0200

    Forbedre ai prompt

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsanalyse/StillingsanalyseTemplate.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsanalyse/StillingsanalyseTemplate.kt
@@ -5,38 +5,48 @@ object StillingsanalyseTemplate {
 
     fun lagPrompt(stillingsanalyseDto: StillingsanalyseController.StillingsanalyseDto): String =
         """
-       Vennligst analyser  stillingsbeskrivelsen og avgjør om den inneholder sensitiv informasjon basert på listen over sensitive ord. Hvis teksten inneholder noen av ordene fra listen, skal den anses som sensitiv med mindre ordene refererer til arbeidsoppgaver, ansvar eller mål for stillingen som skal besettes (for eksempel 'oppfølging av KVP-deltakere' for en 'støttekontakt').
-       Sensitive ord relatert til arbeidsoppgaver, ansvar eller mål direkte knyttet til stillingen skal ikke anses som sensitiv informasjon, og skal i de tilfellene oppføres som sensitiv=false.
+      Instruksjoner:
 
-        **Sensitive ord:**
-        IPS, KVP, Kvalifiseringsprogram, Kvalifiseringslønn, Kvalifiseringsstønad, Aktivitetsplikt, Angst, Arbeid med støtte, Arbeidsevne, Arbeids- og utdanningsreiser, Arbeidsforberedende trening, Arbeidsmarkedskurs, Arbeidspraksis, Arbeidsrettet rehabilitering, Arbeidstrening, AU-reiser, Avklaringstiltak, Barn, Behandling, Behov, Behovsliste, Booppfølging, Deltaker, Depresjon, Diagnoser, Fastlege, Flyktning, Fravær, Gjeld, Helseavklaring, Husleie, Individuell jobbstøtte, Introduksjonsprogram, Introduksjonsstønad, Jobbklar, Jobbspesialist, Kognitive utfordringer, Kognitive problemer, Kognitivt, Kommunale lavterskeltilbud, Kommunale tiltak, Kommunale tjenester, Koordinert bistand, Lån, Langvarig, Livsopphold, Lønnstilskudd, Mentor, Mentortilskudd, Midlertidig bolig, Midlertidig botilbud, Nedsatt arbeidsevne, Nedsatt funksjon, Norskferdigheter, Oppfølging, Oppfølgingstiltak, Oppfølgning i bolig, Opplæring, Opplæringstiltak, Pengestøtte, Problemer, Psykiatri, Psykolog, Rehabilitering, Restanse, Rus, Sommerjobb, Sommerjobbtiltak, Sosial oppfølging, Sosialfaglig oppfølging, Sosialhjelp, Sosialhjelpsmottaker, Sosialstønad, Sosiale problemer, Sosiale utfordringer, Supplerende stønad, Supported Employment, Syk, Sykdom, Sykemeldt, Sykt barn, Tilskudd, Tiltak, Tiltaksdeltaker, Ukrain, Ukraina, Ungdom, Utfordringer, Utvidet oppfølging, Varig tilrettelagt arbeid, Venteliste, Økonomi, Økonomisk kartlegging, Økonomisk rådgivning, Økonomisk sosialhjelp, Økonomisk stønad
+      Analyser stillingsbeskrivelsen og avgjør om den inneholder sensitiv informasjon basert på listen over sensitive ord.
 
-        **Unntak:** Hvis ordet brukes i sammenheng med stillingen som skal besettes (for eksempel "IPS veileder"), skal det ikke anses som sensitivt. Ingenting er sensitivt om det refererer til ønske om en spesialist på et av de sensitive temaene.
+      Sensitiv: Sett "sensitiv": true hvis teksten inneholder noen av de sensitive ordene og disse ordene ikke refererer til arbeidsoppgaver, ansvar eller mål for stillingen som skal besettes.
 
-        **Gi svaret på norsk i følgende JSON-format:**
-        {
-          "sensitiv": true/false,
-          "sensitivBegrunnelse": "Din begrunnelse her."
-          "samsvarMedTittel": true/false,
-          "tittelBegrunnelse": "Din begrunnelse her."
-          "samsvarMedType": true/false,
-          "typeBegrunnelse": "Din begrunnelse her."
-        }
-        
-        samsvarMedTittel referer til om det er samsvar med tittel og stillingstekst
-        samsvarMedType referer til om det er samsvar med stillingstype og stillingstekst
-        jobbmesser og formidlinger har lite krav til innhold i teksten. Men stillingstype stilling, skal være en stillingsannonse, som refererer til en vanlig stilling som kan utlyses.
-        
-        Vennligst svar kun med JSON-objektet uten ytterligere forklaring, og uten kodeblokker eller markdown.
-        
-        **Stillingstittel:**
-        ${stillingsanalyseDto.stillingstittel}
-        
-        **Stillingstype:**
-        ${stillingsanalyseDto.stillingstype}
+      Ikke sensitiv: Sett "sensitiv": false hvis de sensitive ordene refererer til arbeidsoppgaver, ansvar eller mål direkte knyttet til stillingen.
 
-        **Stillingsbeskrivelse:**
-        ${stillingsanalyseDto.stillingstekst}
+      Det er ikke ansett som sensitivt å lyse ut en stilling der man søker en person som skal arbeide med utsatte grupper eller sensitive temaer.
+
+      Sensitive ord:
+
+      IPS, KVP, Kvalifiseringsprogram, Kvalifiseringslønn, Kvalifiseringsstønad, Aktivitetsplikt, Angst, Arbeid med støtte, Arbeidsevne, Arbeids- og utdanningsreiser, Arbeidsforberedende trening, Arbeidsmarkedskurs, Arbeidspraksis, Arbeidsrettet rehabilitering, Arbeidstrening, AU-reiser, Avklaringstiltak, Barn, Behandling, Behov, Behovsliste, Booppfølging, Deltaker, Depresjon, Diagnoser, Fastlege, Flyktning, Fravær, Gjeld, Helseavklaring, Husleie, Individuell jobbstøtte, Introduksjonsprogram, Introduksjonsstønad, Jobbklar, Jobbspesialist, Kognitive utfordringer, Kognitive problemer, Kognitivt, Kommunale lavterskeltilbud, Kommunale tiltak, Kommunale tjenester, Koordinert bistand, Lån, Langvarig, Livsopphold, Lønnstilskudd, Mentor, Mentortilskudd, Midlertidig bolig, Midlertidig botilbud, Nedsatt arbeidsevne, Nedsatt funksjon, Norskferdigheter, Oppfølging, Oppfølgingstiltak, Oppfølgning i bolig, Opplæring, Opplæringstiltak, Pengestøtte, Problemer, Psykiatri, Psykolog, Rehabilitering, Restanse, Rus, Sommerjobb, Sommerjobbtiltak, Sosial oppfølging, Sosialfaglig oppfølging, Sosialhjelp, Sosialhjelpsmottaker, Sosialstønad, Sosiale problemer, Sosiale utfordringer, Supplerende stønad, Supported Employment, Syk, Sykdom, Sykemeldt, Sykt barn, Tilskudd, Tiltak, Tiltaksdeltaker, Ukrain, Ukraina, Ungdom, Utfordringer, Utvidet oppfølging, Varig tilrettelagt arbeid, Venteliste, Økonomi, Økonomisk kartlegging, Økonomisk rådgivning, Økonomisk sosialhjelp, Økonomisk stønad
+
+      Unntak:
+
+      Hvis et sensitivt ord brukes i sammenheng med stillingens oppgaver, ansvar eller kvalifikasjoner (for eksempel "Oppfølging av deltakere på kvalifiseringsprogrammet (KVP)"), skal det ikke anses som sensitivt.
+
+      Svarformat:
+
+      Vennligst svar kun med et JSON-objekt på norsk i følgende format, uten ytterligere forklaringer:
+
+      {
+        "sensitiv": true/false,
+        "sensitivBegrunnelse": "Din begrunnelse her.",
+        "samsvarMedTittel": true/false,
+        "tittelBegrunnelse": "Din begrunnelse her.",
+        "samsvarMedType": true/false,
+        "typeBegrunnelse": "Din begrunnelse her."
+      }
+      samsvarMedTittel: Angi om det er samsvar mellom stillingstittel og stillingstekst.
+      samsvarMedType: Angi om det er samsvar mellom stillingstype og stillingstekst.
+      
+      Merk:
+
+      Jobbmesser og formidlinger har lite krav til innhold i teksten.
+      Stillingsstype "stilling" skal være en stillingsannonse som refererer til en vanlig stilling som kan utlyses.
+      Stillingstittel: ${stillingsanalyseDto.stillingstittel}
+
+      Stillingstype: ${stillingsanalyseDto.stillingstype}
+
+      Stillingsbeskrivelse: ${stillingsanalyseDto.stillingstekst}
         
         """.trimIndent()
 }
