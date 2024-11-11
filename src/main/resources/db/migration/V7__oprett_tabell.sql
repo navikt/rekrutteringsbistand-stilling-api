23a66d7dbfaa623c56419ddbfbf2330c6bcacc77
haugse <sara.haugse@gmail.com>
2024-11-11 10:09:27 +0100

    Ny tabell for interne stillinger

---
+++ src/main/resources/db/migration/V7__oprett_tabell.sql
@@ -0,0 +1,9 @@
+CREATE TABLE intern_stilling (
+    id SERIAL CONSTRAINT PK_SILLING_INTERN PRIMARY KEY,
+    stillingsid UUID NOT NULL UNIQUE,
+    innhold JSONB NOT NULL,
+    opprettet TIMESTAMP DEFAULT current_timestamp,
+    opprettet_av VARCHAR(255) NOT NULL ,
+    sist_endret TIMESTAMP DEFAULT current_timestamp,
+    sist_endret_av VARCHAR(255) NOT NULL
+)
