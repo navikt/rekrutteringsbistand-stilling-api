222b1f49db055ff11cfdb375f7ed021937607dcc
haugse <sara.haugse@gmail.com>
2024-11-11 15:07:13 +0100

    Deploy til dev og time zone på time stamp

--- src/main/resources/db/migration/V7__oprett_tabell.sql
+++ src/main/resources/db/migration/V7__oprett_tabell.sql
@@ -2,8 +2,8 @@ CREATE TABLE intern_stilling (
     id SERIAL CONSTRAINT PK_SILLING_INTERN PRIMARY KEY,
     stillingsid UUID NOT NULL UNIQUE,
     innhold JSONB NOT NULL,
-    opprettet TIMESTAMP DEFAULT current_timestamp,
-    opprettet_av VARCHAR(255) NOT NULL ,
-    sist_endret TIMESTAMP DEFAULT current_timestamp,
+    opprettet TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    opprettet_av VARCHAR(255) NOT NULL,
+    sist_endret TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     sist_endret_av VARCHAR(255) NOT NULL
 )
