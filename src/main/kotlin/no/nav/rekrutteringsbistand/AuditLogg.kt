631d5bfcf6829653234b5ae7996e1c68d2914d84
Joar Aurdal <joar.aurdal@nav.no>
2024-11-20 15:57:33 +0100

    Legg til ekstra space for audit logging, siden auditloggingen kutter i noen tilfeller siste tegn i logmessage

--- src/main/kotlin/no/nav/rekrutteringsbistand/AuditLogg.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/AuditLogg.kt
@@ -35,7 +35,8 @@ object AuditLogg {
             .timeEnded(System.currentTimeMillis())
             .extension("msg", msg)
             .build()
-        auditLogger.log(message)
-        secureLog.info("auditlogger: {}", message)
+        val ekstraSpaceSidenAuditloggerInnimellomKutterSisteTegn = " "
+        auditLogger.log("$message" + ekstraSpaceSidenAuditloggerInnimellomKutterSisteTegn)
+        secureLog.info("auditlogger: {}", "$message" + ekstraSpaceSidenAuditloggerInnimellomKutterSisteTegn)
     }
 }
