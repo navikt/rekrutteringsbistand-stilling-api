501d5fea8882997945747cd6d1dcbcd96b1ac38d
Are Husby <are.husby@nav.no>
2024-12-11 13:04:56 +0100

    Mer kontekst i logging av forbidden respons + fiks deprecation warnings

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/support/rest/RestResponseEntityExceptionHandler.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/support/rest/RestResponseEntityExceptionHandler.kt
@@ -1,5 +1,6 @@
 package no.nav.rekrutteringsbistand.api.support.rest
 
+import jakarta.servlet.http.HttpServletRequest
 import no.nav.rekrutteringsbistand.api.support.log
 import no.nav.security.token.support.core.exceptions.JwtTokenValidatorException
 import org.springframework.dao.EmptyResultDataAccessException
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.client.RestClientResponseException
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import jakarta.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
@@ -28,7 +28,7 @@ class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
     @Deprecated("Bruk arrow.core.Option eller en tom collection istedenfor. Exceptions for kontrollflyt som ikke er feilsituasjoner er et anti-pattern.")
     protected fun handleNoContent(e: RuntimeException, request: HttpServletRequest): ResponseEntity<Any> {
         val uri = request.requestURI
-        log.info("No content found at requestURI=${request.requestURI}, HTTP method=${request.method}")
+        log.info("No content found at requestURI=${request.requestURI}, HTTP method=${request.method}", e)
         return ResponseEntity.status(NO_CONTENT).body(uri)
     }
 
@@ -39,12 +39,12 @@ class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
     protected fun handleExceptionFraRestTemplate(
         e: RestClientResponseException, request: HttpServletRequest
     ): ResponseEntity<String> {
-        val msg = "Mottok HTTP respons ${e.rawStatusCode} fra ${request.method} mot URL ${request.requestURL}"
-        when (e.rawStatusCode) {
+        val msg = "Mottok HTTP respons ${e.statusCode} fra ${request.method} mot URL ${request.requestURL}"
+        when (e.statusCode.value()) {
             403 -> log.warn(msg, e)
             404 -> log.info(msg, e)
             else -> log.error(msg, e)
         }
-        return ResponseEntity.status(e.rawStatusCode).body(e.responseBodyAsString)
+        return ResponseEntity.status(e.statusCode.value()).body(e.responseBodyAsString)
     }
 }
