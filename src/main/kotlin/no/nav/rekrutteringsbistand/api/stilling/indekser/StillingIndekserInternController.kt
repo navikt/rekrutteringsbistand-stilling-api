0411b80c5a9413deb33d805901538fa1f3aebef3
haugse <sara.haugse@gmail.com>
2025-03-28 10:14:00 +0100

    Legger til et endepunkt for test av indekser

---
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stilling/indekser/StillingIndekserInternController.kt
@@ -0,0 +1,48 @@
+package no.nav.rekrutteringsbistand.api.stilling.indekser
+
+import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
+import no.nav.rekrutteringsbistand.api.hendelser.RapidApplikasjon
+import no.nav.rekrutteringsbistand.api.stilling.StillingService
+import no.nav.rekrutteringsbistand.api.stillingsinfo.Stillingsid
+import no.nav.rekrutteringsbistand.api.stillingsinfo.StillingsinfoService
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/internal/stilling")
+@Unprotected
+class StillingIndekserInternController(
+    val stillingService: StillingService,
+    val rapidApplikasjon: RapidApplikasjon,
+    val stillingsinfoService: StillingsinfoService
+) {
+
+    @PostMapping("/reindekser")
+    fun reindekser(@RequestBody stillingsId: String): ResponseEntity<String> {
+        val uuid: UUID
+        try {
+            uuid = UUID.fromString(stillingsId)
+        } catch (e: Exception) {
+            return ResponseEntity("Fikk ikke til å konvertere til uuid", HttpStatus.BAD_REQUEST)
+        }
+        val enStilling = stillingService.hentRekrutteringsbistandStilling(uuid.toString(), true)
+        val packet = JsonMessage.newMessage(eventName = "direktemeldtStillingRepubliser")
+
+        packet["stilling"] = enStilling.stilling
+        packet["stillingsinfo"] = stillingsinfoService.hentForStilling(stillingId = Stillingsid(uuid)) ?: ""
+        packet["stillingsId"] = uuid.toString()
+
+        packet["direktemeldtStilling"] = stillingService.hentDirektemeldtStilling(uuid.toString())
+
+        rapidApplikasjon.publish(Stillingsid(uuid), packet)
+
+        return ResponseEntity.ok("Stilling $uuid er lagt på rapid")
+    }
+
+}
