3f7a8ed965ce630482ed9e9e8e26fbfeb2eeb37f
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-05-16 14:49:12 +0200

    Endre også innholdet i json strengen

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingStatusoppdatering/FjernAdministrationPendingJobb.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingStatusoppdatering/FjernAdministrationPendingJobb.kt
@@ -2,7 +2,6 @@ package no.nav.rekrutteringsbistand.api.stillingStatusoppdatering
 
 import no.nav.rekrutteringsbistand.api.stilling.AdminStatus
 import no.nav.rekrutteringsbistand.api.stilling.DirektemeldtStillingRepository
-import no.nav.rekrutteringsbistand.api.stilling.Status
 import no.nav.rekrutteringsbistand.api.support.config.LeaderElection
 import no.nav.rekrutteringsbistand.api.support.log
 import org.springframework.scheduling.annotation.Scheduled
@@ -21,11 +20,10 @@ class FjernAdministrationPendingJobb(
     fun aktiverOgDeaktiverStillingerJobb() {
         // Sjekker om det er leader, slik at jobben kun kjører på en pod
         if (leaderElection.isLeader()) {
-            log.info("Startet jobb for å sette Adminstatus til Done")
+            log.info("Startet jobb for å sette Adminstatus til DONE")
             val stillingerSomSkalSettesTilDone = direktemeldtStillingRepository.hentUtgåtteStillingerFor6mndSidenSomErPending()
-            log.info("Fant ${stillingerSomSkalSettesTilDone.size} stillinger som skal settes til Done")
+            log.info("Fant ${stillingerSomSkalSettesTilDone.size} stillinger som skal settes til AdminStatus DONE")
 
-            // TODO: Kommenter inn når spørringen over er verifisert
             stillingerSomSkalSettesTilDone.forEach {
                 val stillingNyAdminStatus = it.copy(
                     adminStatus = AdminStatus.DONE.toString(),
