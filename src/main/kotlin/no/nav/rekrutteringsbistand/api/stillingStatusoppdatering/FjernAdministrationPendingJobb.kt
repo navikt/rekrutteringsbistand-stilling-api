d022f506d8a74e77f31ee3e43f22981477e26a8d
Ken Andersen <ken.ove.andersen@nav.no>
2025-05-19 12:16:49 +0200

    Remove pending job (#272)
    * Endre AdminStatus til DONE for annonser eldre en 6mnd
    
    * Legg til en dag for å gjøre testen mer robust
    
    * Deploy til dev
    
    * Kommenter inn koden som setter AdminStatus til DONE
    
    * Endre også innholdet i json strengen
    
    * Endre også innholdet i json strengen
    
    * Kjør jobb kun en gang om dagen
    
    * Endre navn på metode

---
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingStatusoppdatering/FjernAdministrationPendingJobb.kt
@@ -0,0 +1,38 @@
+package no.nav.rekrutteringsbistand.api.stillingStatusoppdatering
+
+import no.nav.rekrutteringsbistand.api.stilling.AdminStatus
+import no.nav.rekrutteringsbistand.api.stilling.DirektemeldtStillingRepository
+import no.nav.rekrutteringsbistand.api.support.config.LeaderElection
+import no.nav.rekrutteringsbistand.api.support.log
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+
+@Service
+class FjernAdministrationPendingJobb(
+    private val direktemeldtStillingRepository: DirektemeldtStillingRepository,
+    private val leaderElection: LeaderElection
+) {
+
+    @Scheduled(cron = "0 30 0 * * *")
+    fun fjernAdministrationPendingJobb() {
+        // Sjekker om det er leader, slik at jobben kun kjører på en pod
+        if (leaderElection.isLeader()) {
+            log.info("Startet jobb for å sette Adminstatus til DONE")
+            val stillingerSomSkalSettesTilDone = direktemeldtStillingRepository.hentUtgåtteStillingerFor6mndSidenSomErPending()
+            log.info("Fant ${stillingerSomSkalSettesTilDone.size} stillinger som skal settes til AdminStatus DONE")
+
+            stillingerSomSkalSettesTilDone.forEach {
+                val stillingNyAdminStatus = it.copy(
+                    adminStatus = AdminStatus.DONE.toString(),
+                    sistEndret = ZonedDateTime.now(ZoneId.of("Europe/Oslo")),
+                    innhold = it.innhold.copy(administration = it.innhold.administration?.copy(status = AdminStatus.DONE.toString())),
+                )
+                direktemeldtStillingRepository.lagreDirektemeldtStilling(stillingNyAdminStatus)
+                log.info("Satt AdminStatus DONE for ${it.stillingsId} pga eldre enn 6mnd")
+            }
+        }
+    }
+}
