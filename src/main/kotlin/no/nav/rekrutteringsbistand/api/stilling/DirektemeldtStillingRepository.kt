6ff6fd20d541da7e2fd48cca043cc263514db83b
Ken Ove Andersen <ken.ove.andersen@nav.no>
2025-06-02 14:16:39 +0200

    Sett limit tilbake til 5000
    Co-authored-by: Sara <43448582+haugse@users.noreply.github.com>

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stilling/DirektemeldtStillingRepository.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stilling/DirektemeldtStillingRepository.kt
@@ -191,7 +191,7 @@ class DirektemeldtStillingRepository(private val namedJdbcTemplate: NamedParamet
                 $DIREKTEMELDT_STILLING_TABELL
             where 
                 $ANNONSENR is null
-            LIMIT 10000
+            LIMIT 5000
         """.trimIndent()
 
         return namedJdbcTemplate.query(
