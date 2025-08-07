f2dcd15e0335d64092dbfa773d4ed779d50bcc51
haugse <sara.haugse@gmail.com>
2025-08-07 14:05:46 +0200

    Legg til replication_slot
    Co-authored-by: Vilde Stefferud <33753494+vildestefferud@users.noreply.github.com>

--- src/main/resources/db/migration/V16__oprett_datastream_replication_slot.sql
+++ src/main/resources/db/migration/V16__oprett_datastream_replication_slot.sql
@@ -1,1 +1,8 @@
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT ('toi_rekrutteringsbistand_stilling_api_replication', 'pgoutput');
+DO
+$$
+    BEGIN
+        IF EXISTS(SELECT * FROM pg_roles WHERE rolname = 'bigquery_datastream') THEN
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT('toi_rekrutteringsbistand_stilling_api_replication', 'pgoutput');
+        END IF;
+    END
+$$
