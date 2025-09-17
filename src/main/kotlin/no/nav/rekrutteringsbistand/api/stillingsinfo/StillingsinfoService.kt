88a5aa5eb3540cd0412e86f48aa36c2453b3a182
haugse <sara.haugse@gmail.com>
2025-09-17 12:35:34 +0200

    Legg melding i outboxen ved overta ekstern stilling som skal oppdatere stillingsinfo i opensearch

--- src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoService.kt
+++ src/main/kotlin/no/nav/rekrutteringsbistand/api/stillingsinfo/StillingsinfoService.kt
@@ -5,6 +5,8 @@ import no.nav.rekrutteringsbistand.api.kandidatliste.KandidatlisteDto
 import no.nav.rekrutteringsbistand.api.kandidatliste.KandidatlisteKlient
 import no.nav.rekrutteringsbistand.api.kandidatliste.KandidatlisteStillingDto
 import no.nav.rekrutteringsbistand.api.stilling.DirektemeldtStilling
+import no.nav.rekrutteringsbistand.api.stilling.outbox.EventName
+import no.nav.rekrutteringsbistand.api.stilling.outbox.StillingOutboxService
 import no.nav.rekrutteringsbistand.api.support.log
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -18,7 +20,8 @@ import java.util.concurrent.TimeUnit
 class StillingsinfoService(
     private val repo: StillingsinfoRepository,
     private val kandidatlisteKlient: KandidatlisteKlient,
-    private val arbeidsplassenKlient: ArbeidsplassenKlient
+    private val arbeidsplassenKlient: ArbeidsplassenKlient,
+    private val stillingOutboxService: StillingOutboxService
 ) {
 
     @Transactional
@@ -64,8 +67,7 @@ class StillingsinfoService(
         } catch (e: Exception) {
             throw RuntimeException("Varsel til rekbis-kandidat-api om endring av eier for ekstern stilling feilet", e)
         }
-
-        arbeidsplassenKlient.triggResendingAvStillingsmeldingFraArbeidsplassen(stillingsId.asString())
+        stillingOutboxService.lagreMeldingIOutbox(stillingsId = UUID.fromString(stillingsId.toString()), eventName = EventName.INDEKSER_STILLINGSINFO)
         return stillingsinfoMedNyEier
     }
 
